cb9874e3423851cd307aa58055d1556e0780c517
makeychik <mikitamakeichyk@gmail.com>
2021-03-10 23:51:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsArray = bracketsConfig.map((bracketsCouple) =>
+        bracketsCouple.join("")
+    );
+
+    while (str.length > 0) {
+        let currentString = str;
+
+        for (let brackets of bracketsArray) {
+            currentString = currentString.replace(brackets, "");
+        }
+
+        if (str === currentString) {
+            return false;
+        }
+
+        str = currentString;
+    }
+    return true;
+};
